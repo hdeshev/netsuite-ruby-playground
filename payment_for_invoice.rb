43c48f3122fd13e378a2e4d94cd8be35965e527e
Hristo Deshev <hristo@deshev.com>
2017-08-31 15:54:11 +0300

    Create payment from invoice, associate it with an account.

--- payment_for_invoice.rb
+++ payment_for_invoice.rb
@@ -2,15 +2,17 @@ require 'pp'
 require 'date'
 require_relative './config'
 
-sales_order = NetSuite::Records::SalesOrder.get(2420)
-#pp sales_order
+invoice = NetSuite::Records::Invoice.get(2520)
+pp invoice.status
 
-new_invoice = NetSuite::Records::Invoice.initialize(sales_order)
-#
+new_payment = NetSuite::Records::CustomerPayment.initialize(invoice)
+new_payment.account = NetSuite::Records::RecordRef.new(internal_id: 217)
+new_payment.payment_method = NetSuite::Records::RecordRef.new(internal_id: 7)
+#pp new_payment
+
 # work around a stupid crash here:
 # <platformCore:code>INSUFFICIENT_PERMISSION</platformCore:code>
 # <platformCore:message>You do not have permissions to set a value for element exchangerate due to one of the following reasons: 1) The field is read-only; 2) An associated feature is disabled; 3) The field is available either when a record is created or updated, but not in both cases.</platformCore:message>
-new_invoice.exchange_rate = nil
+new_payment.exchange_rate = nil
 
-#pp new_invoice
-pp new_invoice.add
+pp new_payment.add
