291e03d75ab9cb480b089a4501d4ecbaf01c584e
Hristo Deshev <hristo@deshev.com>
2017-09-08 22:52:33 +0300

    Create edit/customer

--- customers.rb
+++ customers.rb
@@ -5,8 +5,39 @@ def get_customer(id)
   NetSuite::Records::Customer.get id
 end
 
-customer = get_customer 3
-puts "#{customer.internal_id},#{customer.external_id},#{customer.email}"
 
-#customer.external_id = '6102679841'
-#customer.update
+def update_customer(id)
+  customer = get_customer id
+  customer.external_id = '6102679841'
+  customer.update
+end
+
+def create_customer()
+  new_customer = NetSuite::Records::Customer.new(
+    account_number: '6422657874',
+    company_name: 'Example Dot Com',
+    email: 'mycustomer@example.com',
+    phone: '+44 12345678',
+    is_person: false,
+    subsidiary: {internal_id: 3, name: 'USA'},
+    vat_reg_number: '1234567890',
+  )
+  new_customer.add
+  new_customer
+end
+
+def change_subsidiary(id, new_subsidiary)
+  customer = get_customer id
+  puts "Current subsidiary: #{customer.subsidiary.internal_id}"
+  customer.update(subsidiary: {internal_id: new_subsidiary})
+  customer
+end
+
+#customer = get_customer 111
+#puts "#{customer.internal_id},#{customer.external_id},#{customer.email}"
+
+#new_customer = create_customer
+#puts "ID: #{new_customer.internal_id} #{new_customer.errors}"
+
+customer = change_subsidiary 511, 3
+puts "#{customer.subsidiary.internal_id}, #{customer.errors}"
